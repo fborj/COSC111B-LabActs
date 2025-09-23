11b5e2b22c53b004e36414ca3645e1fe0ae9e538
borjf <127951786+fborj@users.noreply.github.com>
2025-09-23 20:17:50 +0800

    Add files via upload

---
+++ LabAct2.ino
@@ -0,0 +1,38 @@
+int ledPins[5] = {12, 11, 10, 9, 8};
+int numLeds = 5;
+
+void setup() {
+  int i = 0;
+  while (i < numLeds) {
+    pinMode(ledPins[i], OUTPUT);
+    i++;
+  }
+}
+
+void loop() {
+  int brightness = 0;
+  int fadeAmount = 50; 
+  int onIndex = 0;
+  while (onIndex < numLeds) {
+    brightness = 0;
+    while (brightness <= 255) {
+      analogWrite(ledPins[onIndex], brightness);
+      brightness += fadeAmount;
+      delay(20); 
+    }
+    delay(1000); 
+    onIndex++;
+  }
+
+  int offIndex = 0;
+  while (offIndex < numLeds) {
+    brightness = 255;
+    while (brightness >= 0) {
+      analogWrite(ledPins[offIndex], brightness);
+      brightness -= fadeAmount;
+      delay(20); 
+    }
+    delay(1000);
+    offIndex++;
+  }
+}
