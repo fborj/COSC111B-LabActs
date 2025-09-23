580ac92cbfe62bcfca7c6ea1e4c5c5b8aeaa2998
borjf <127951786+fborj@users.noreply.github.com>
2025-09-23 20:43:00 +0800

    Add files via upload

--- LabAct2.ino
+++ LabAct2.ino
@@ -1,4 +1,4 @@
-int ledPins[5] = {12, 11, 10, 9, 8};
+int ledPins[5] = {11, 10, 9, 6, 5};
 int numLeds = 5;
 
 void setup() {
@@ -11,16 +11,17 @@ void setup() {
 
 void loop() {
   int brightness = 0;
-  int fadeAmount = 50; 
+  int fadeAmount = 50;
+
   int onIndex = 0;
   while (onIndex < numLeds) {
     brightness = 0;
     while (brightness <= 255) {
       analogWrite(ledPins[onIndex], brightness);
       brightness += fadeAmount;
-      delay(20); 
+      delay(20);
     }
-    delay(1000); 
+    delay(1000);
     onIndex++;
   }
 
@@ -30,7 +31,7 @@ void loop() {
     while (brightness >= 0) {
       analogWrite(ledPins[offIndex], brightness);
       brightness -= fadeAmount;
-      delay(20); 
+      delay(20);
     }
     delay(1000);
     offIndex++;
